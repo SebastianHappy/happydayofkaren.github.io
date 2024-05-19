6bd0ed7757e9813e456cd5766d5799c0a90c9cb1
Sebastian <110787247+SebastianHappy@users.noreply.github.com>
2024-05-18 19:37:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,14 @@ function updateCountdown() {
     const currentYear = now.getFullYear();
     const targetDate = new Date(`May 18, ${currentYear} 00:00:00`);
     
- 
     if (now.getMonth() === 4 && now.getDate() === 18) {
         document.getElementById('message').textContent = '¡Feliz Cumpleaños Karen!!!';
         document.getElementById('countdown').textContent = '';
         return; 
     } else {
-        document.getElementById('message').textContent = '';
+        document.getElementById('message').textContent = 'Faltan:';
     }
     
-
     if (now > targetDate) {
         targetDate.setFullYear(currentYear + 1);
     }
@@ -23,13 +21,9 @@ function updateCountdown() {
     const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
 
- 
     document.getElementById('countdown').textContent = `${days} días ${hours} horas ${minutes} minutos`;
 }
 
-
 setInterval(updateCountdown, 1000);
 
-
 updateCountdown();
-
